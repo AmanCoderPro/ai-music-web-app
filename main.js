fa7f6ab2f394bd79d910230595165e8f28d36742
AmanCoderPro <87359434+AmanCoderPro@users.noreply.github.com>
2023-01-03 17:27:05 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,23 @@ song2 = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
-rigihtWristY = 0;
+rightWristY = 0;
+scorerightWrist = 0;
 scoreleftWrist = 0;
-songstatus = "";
+song1status = "";
+song2status = "";
+
+function play() {
+    song1.setVolume(1);
+    song1.rate(1);
+    song1.play();
+    song2.setVolume(1);
+    song2.rate(1);
+}
 
 function preload() {
     song1 = loadSound("music.mp3");
     song2 = loadSound('Ishowspeed-World-Cup.mp3');
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
@@ -24,6 +32,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
@@ -32,7 +43,18 @@ function draw() {
     fill("#1B78EE");
     stroke("#1B78EE");
 
-    songstatus = song1.isPlaying();
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();
+
+        if(scorerightWrist > 0.2) {
+            circle(rightWristX, rightWristY, 20);
+            song1.stop();
+        }
+
+        if(song2status == false) {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song = Harry Potter";
+        }
 
     if(scoreleftWrist > 0.2) {
         
@@ -40,12 +62,11 @@ function draw() {
 
         InNumberleftWristY = Number(leftWristY);
         song2.stop();
-        song1.isPlaying(true);
     }
 
-    if(songstatus == false) {
-        song1.isPlaying(true);
-        document.getElementById("song").innerHTML ="Song = " + "Harry Potter";
+    if(song1status == false) {
+        song1.play();
+        document.getElementById("song_name").innerHTML ="Song = World Cup";
     }
 }
 
@@ -53,6 +74,7 @@ function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
 
+        scorerightWrist = results[0].pose.keypoints[10].score;
         scoreleftWrist = results[0].pose.keypoints[9].score;
 
         leftWristX = results[0].pose.leftWrist.x;
